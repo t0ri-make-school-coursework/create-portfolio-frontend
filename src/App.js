dc106eb0fabe3a664211a70769cb12f2735df6fe
t0ri <victoriarosemurray@gmail.com>
2019-02-20 12:45:31 -0800

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css'
+
+import ProjectIndex from './Routes/ProjectIndex/ProjectIndex.jsx'
+import ProjectShow from './Routes/ProjectShow/ProjectShow.jsx'
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+        <Router>
+          <Route path="/projects" exact component={ProjectIndex} />
+          <Route 
+            path="${match.url}/projectName"
+            render={() => <ProjectShow project="${projectName}"
+          />
+        </Router>
       </div>
     )
   }
