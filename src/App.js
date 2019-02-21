58f8eec58090ae578854ea9edbea07e05689c5c2
t0ri <victoriarosemurray@gmail.com>
2019-02-21 13:21:38 -0800

    proxying to api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css'
 
-const fetch = require('node-fetch')
-
 import ProjectIndex from './Routes/ProjectIndex/ProjectIndex.jsx'
 // import ProjectShow from './Routes/ProjectShow/ProjectShow.jsx'
 
+const fetch = require('node-fetch')
 
 class App extends React.Component {
 
+  componentDidMount() {
+    fetch('/api/projects').then((res) => {
+      return res.json()
+    }).then((json) => {
+      const { projects } = json
+      console.log(projects)
+      // const { about } = json // Get a value from JSON object
+      // this.setState({ about }) // Set a value on state with returned value
+    }).catch((err, json) => {
+      // Handle errors
+      console.log(err.message)
+    })
+  }
   
   render() {
     return (
