00ce663ad640a8817073db2369c5e4a7c62c9cb0
t0ri <victoriarosemurray@gmail.com>
2019-02-21 11:51:34 -0800

    create and style projectindex

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css'
 
+const fetch = require('node-fetch')
+
 import ProjectIndex from './Routes/ProjectIndex/ProjectIndex.jsx'
-import ProjectShow from './Routes/ProjectShow/ProjectShow.jsx'
+// import ProjectShow from './Routes/ProjectShow/ProjectShow.jsx'
 
 
 class App extends React.Component {
+
+  
   render() {
     return (
       <div className="App">
         <Router>
-          <Route path="/projects" exact component={ProjectIndex} />
-          <Route 
-            path="${match.url}/projectName"
-            render={() => <ProjectShow project="${projectName}"
-          />
+          <div>
+            <Route path="/projects" component={ProjectIndex} />
+            <Route path="/" exact component={ProjectIndex} />
+            {/* <Route 
+              path="${match.url}/projectName"
+              render={() => <ProjectShow project="${projectName}" />}
+            /> */}
+          </div>
         </Router>
       </div>
     )
---
+++ src/Routes/ProjectIndex/ProjectIndex.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './projectindex.css'
+
+export default class ProjectIndex extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div id="project-index-container">
+        <h1>Projects</h1>
+        <div id="projects-container">
+          cool
+        </div>
+      </div>
+    )
+  }
+}
