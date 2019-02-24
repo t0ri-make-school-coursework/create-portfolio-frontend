0c8b0f6a391171db37434979dda8fd43c67e6e8b
t0ri <victoriarosemurray@gmail.com>
2019-02-23 18:03:32 -0800

    made ProjectCard component

--- src/Routes/ProjectIndex/Components/ProjectCard/ProjectCard.jsx
+++ src/Routes/ProjectIndex/Components/ProjectCard/ProjectCard.jsx
@@ -3,9 +3,23 @@ import './projectcard.css'
 
 export default class ProjectCard extends React.Component {
   render() {
+    const {
+      image,
+      title,
+      shortDesc,
+      class: madeFor,
+      tech,
+      live,
+      repo
+    } = this.props.project
     return (
       <div className="project-card">
-        <h1>cool, {this.props.project.title}</h1>
+        <h1>{title}</h1>
+        <p>{shortDesc}</p>
+        <p>Made for {madeFor}</p>
+        <p>Tech: {tech}</p>
+        <a href={live} target="_blank">View Live</a>
+        <a href={repo} target="_blank">View Code</a>
       </div>
     )
   }
